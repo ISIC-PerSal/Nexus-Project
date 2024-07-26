68e1a9a38811121cbd6107bb8aa918f80d1a9626
JLeonardo96 <jesusleonardorc102@gmail.com>
2024-07-26 16:08:17 -0600

    Cambios3

--- src/components/NewProjectForm/NewProjectForm.jsx
+++ src/components/NewProjectForm/NewProjectForm.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import NewProjectFormView from './NewProjectFormView'
 
 function NewProjectForm() {
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
   return (
    <>
    <NewProjectFormView/>
