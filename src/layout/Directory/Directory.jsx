2e8887c332ee227a6b7fa4f0c90a3a749239fa20
JLeonardo96 <jesusleonardorc102@gmail.com>
2024-07-29 19:57:37 -0600

    Cambios

--- src/layout/Directory/Directory.jsx
+++ src/layout/Directory/Directory.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import DirectoryView from './DirectoryView'
 import NavbarGuest from '../../components/Navbar/NavbarGuest'
+import isAuth from '../../util/isAuth'
+import Navbar from '../../components/Navbar/Navbar'
 
 function Directory() {
+    const isLogin= isAuth()
+    console.log(isLogin)
+
     return (
         <>
-            <NavbarGuest />
+            {
+                isLogin? (<Navbar/>): (<NavbarGuest/>)
+            }
+            
             <DirectoryView />
         </>
     )
---
+++ src/util/odsData.js
@@ -0,0 +1,88 @@
+const odsData = [
+    {
+        ods: 1,
+        title: "Fin de la pobreza",
+        color: "#173567",
+    },
+    {
+        ods: 2,
+        title: "Hambre cero",
+        color: "#EA1C2D",
+    },
+    {
+        ods: 3,
+        title: "Salud y bienestar",
+        color: "#D3A028",
+    },
+    {
+        ods: 4,
+        title: "Educación de calidad",
+        color: "#3CA643",
+    },
+    {
+        ods: 5,
+        title: "Igualdad de género",
+        color: "#C21E33",
+    },
+    {
+        ods: 6,
+        title: "Agua limpia y saneamiento",
+        color: "#EE402A",
+    },
+    {
+        ods: 7,
+        title: "Energía asequible y no contaminante",
+        color: "#00ADD9",
+    },
+    {
+        ods: 8,
+        title: "Trabajo decente y crecimiento económico",
+        color: "#FCB712",
+    },
+    {
+        ods: 9,
+        title: "Industria, innovación e infraestructura",
+        color: "#A21942",
+    },
+    {
+        ods: 10,
+        title: "Reducción de las desigualdades",
+        color: "#F36D25",
+    },
+    {
+        ods: 11,
+        title: "Ciudades y comunidades sostenibles",
+        color: "#E01383",
+    },
+    {
+        ods: 12,
+        title: "Producción y consumo responsables",
+        color: "#CE8C29",
+    },
+    {
+        ods: 13,
+        title: "Acción por el clima",
+        color: "#48773D",
+    },
+    {
+        ods: 14,
+        title: "Vida submarina",
+        color: "#007DBC",
+    },
+    {
+        ods: 15,
+        title: "Vida de ecosistemas terrestres",
+        color: "#3DB048",
+    },
+    {
+        ods: 16,
+        title: "Paz, justicia e instituciones sólidas",
+        color: "#02558B",
+    },
+    {
+        ods: 17,
+        title: "Alianzas para lograr los objetivos",
+        color: "#183568",
+    },
+]
+export default odsData;
