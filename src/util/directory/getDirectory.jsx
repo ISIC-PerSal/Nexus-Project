bfaeb02b5085d8043daab323309dd87914600828
ErickPaul56 <isic22.ereyest@itesco.edu.mx>
2024-08-02 17:21:58 -0600

    fetch de directorio

---
+++ src/util/directory/getDirectory.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import getDataApi from '../getDataApi';
+const API_NEXUS_PROJECT= import.meta.env.VITE_API_NEXUS_PROJECT_DATABASE;
+
+async function getDirectory(pais, ods) {
+    const url = `${API_NEXUS_PROJECT}/directorio/getDirectorio.php?pais=${pais}&ods=${ods}`;
+    try {
+        const data = await getDataApi(url, "");
+        return data
+    } catch (error) {
+        console.error("Error: ", error.message)
+        return null
+    }
+}
+
+export default getDirectory
