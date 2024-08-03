f7a92e57cd9bae40e2c185097073501388e65d97
ErickPaul56 <isic22.ereyest@itesco.edu.mx>
2024-08-02 19:48:06 -0600

    cambios error

---
+++ src/components/BadgeODS/BadgeODSView.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import odsData from '../../util/odsData'
+
+function BadgeODSView({item}) {
+  return (
+    <>
+      <span
+          style={{ backgroundColor: odsData[item.id_ods - 1].color }}
+          className="badge"
+        >
+          {item.ods}
+        </span>
+    </>
+  )
+}
+
+export default BadgeODSView
---
+++ src/components/CardODS/CardODS.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import CardODSView from "./CardODSView";
+
+function CardODS({ item }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
+  return (
+    <>
+      <CardODSView item={item} toggleDescription={toggleDescription} isExpanded={isExpanded}/>
+    </>
+  );
+}
+
+export default CardODS;
