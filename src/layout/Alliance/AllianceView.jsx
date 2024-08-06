6b1d14c07e9131b349a278d9fbbf0bac2c027553
JLeonardo96 <jesusleonardorc102@gmail.com>
2024-08-05 18:02:19 -0600

    Nuevo alianza

---
+++ src/layout/Alliance/AllianceView.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function AllianceView() {
+    return (
+        <>
+            <main className="bg-img py-3">
+                <div className="container w-50 p-3 border rounded border-secondary-subtle bg-gral">
+               <div className='fs-3 fw-bold'>
+                    Alianza juvenil e infantil
+               </div>
+               <div>
+                <p className='text-align-justify p-2'>
+                    En Nexus Project creemos que todos los aliados aportan algo
+                    a su causa cuando se les incluye sin barreras, y en este 
+                    mundo maravilloso coexistimos personas de todas las edades.
+                    Los objetivos para una vida sostenible nos conciernen a todas y todos
+                    por igual, por lo que, a través de una interfaz personalizada es que 
+                    adolescentes, niñas y niños pueden acceder a la red de iniciativas, 
+                    ser líderes o miembros, en un movimiento que por su lado es impulsado
+                    por y para otros jóvenes e infantes.
+                </p>
+               </div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default AllianceView
