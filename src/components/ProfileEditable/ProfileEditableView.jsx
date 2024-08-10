ef121ef7cb511b42ba499552f5bb0da9b8b97d4b
ISIC-PerSal <isic22.rpnrezs@itesco.edu.mx>
2024-08-09 19:35:13 -0600

    editable view

--- src/components/ProfileEditable/ProfileEditableView.jsx
+++ src/components/ProfileEditable/ProfileEditableView.jsx
@@ -1,99 +1,150 @@
-import React from 'react'
-import Tooltip from '@mui/material/Tooltip';
-import HelpCenterIcon from '@mui/icons-material/HelpCenter';
+import React from "react";
+import Tooltip from "@mui/material/Tooltip";
+import HelpCenterIcon from "@mui/icons-material/HelpCenter";
 
-function ProfileEditableView({ name, setName, lastName, setlastName, email, setEmail, birthday, setBirthday, password, setPassword, rfc, setRfc, clabe, setClabe, updateUser }) {
-    const textCLABE = `¿Por qué preguntamos esto?: Para tener un seguimiento seguro de donativos financieros en temas legales, solicitamos este tipo de datos a nuestros usuarios. No se comparten con nadie más`;
-    return (
-        <>
-            <form
-                action=""
-                className="container w-50 mt-3 p-3 border rounded border-secondary-subtle bg-gral ms-4"
+function ProfileEditableView({
+  name,
+  setName,
+  lastName,
+  setLastName,
+  email,
+  setEmail,
+  birthday,
+  setBirthday,
+  password,
+  setPassword,
+  rfc,
+  setRfc,
+  clabe,
+  setClabe,
+  updateUser,
+}) {
+  const textCLABE = `¿Por qué preguntamos esto?: Para tener un seguimiento seguro de donativos financieros en temas legales, solicitamos este tipo de datos a nuestros usuarios. No se comparten con nadie más`;
+  return (
+    <>
+      <form
+        action=""
+        className="container w-50 mt-3 p-3 border rounded border-secondary-subtle bg-gral ms-4"
+      >
+        <div>
+          <h2>
+            Editar perfil{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fillRule="currentColor"
+              className="bi bi-pencil-square"
+              viewBox="0 0 16 16"
             >
-                <div>
-                    <h2>Editar perfil{" "}
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
-                        </svg>
-                    </h2>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="nameUser" class="form-label">
-                        Nombre
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="nameUser"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    ></input>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="lastNameUser" class="form-label">
-                        Apellidos
-                    </label>
-                    <input type="text" className="form-control" id="lastNameUser"
-                        value={lastName}
-                        onChange={(e) => setlastName(e.target.value)}
-                    ></input>
-                </div>
-                <div class="mb-3">
-                    <label htmlFor="emailUser" className="form-label">
-                        Correo
-                    </label>
-                    <input type="email" class="form-control" id="emailUser"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    ></input>
-                </div>
-                <div class="mb-3">
-                    <label htmlFor="birthdayUser" className="form-label">
-                        Fecha de nacimiento
-                    </label>
-                    <input disabled={true} type="date" class="form-control" id="birthdayUser"
-                        value={birthday}
-                        onChange={(e) => setBirthday(e.target.value)}
-                    ></input>
-                </div>
-                <div class="mb-3">
-                    <label htmlFor="passwordUser" className="form-label">
-                        Contraseña
-                    </label>
-                    <input type="text" class="form-control" id="passwordUser"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    ></input>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="rfcUser" className="form-label">
-                        RFC{" "}
-                        <Tooltip title={textCLABE}>
-                            <HelpCenterIcon />
-                        </Tooltip>
-                    </label>
-                    <input type="text" className="form-control" id="rfcUser"
-                    value={rfc}
-                    onChange={(e) => setRfc(e.target.value)}
-                    ></input>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="clabeUser" className="form-label">
-                        CLABE{" "}
-                        <Tooltip title={textCLABE}>
-                            <HelpCenterIcon />
-                        </Tooltip>
-                    </label>
-                    <input type="text" className="form-control" id="clabeUser"
-                    value={clabe}
-                    onChange={(e) => setClabe(e.target.value)}
-                    ></input>
-                </div>
-                <input onClick={updateUser} class="btn btn-primary w-100" type="submit" value="Guardar datos"></input>
-            </form>
-        </>
-    )
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+              <path
+                fill-rule="evenodd"
+                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+              />
+            </svg>
+          </h2>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="nameUser" className="form-label">
+            Nombre
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="nameUser"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="lastNameUser" className="form-label">
+            Apellidos
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastNameUser"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="emailUser" className="form-label">
+            Correo
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="emailUser"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="birthdayUser" className="form-label">
+            Fecha de nacimiento
+          </label>
+          <input
+            disabled={true}
+            type="date"
+            className="form-control"
+            id="birthdayUser"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="passwordUser" className="form-label">
+            Contraseña
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="passwordUser"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="rfcUser" className="form-label">
+            RFC{" "}
+            <Tooltip title={textCLABE}>
+              <HelpCenterIcon />
+            </Tooltip>
+          </label>
+          <input
+            value={rfc}
+            onChange={(e) => setRfc(e.target.value)}
+            type="text"
+            className="form-control"
+            id="rfcUser"
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="clabeUser" className="form-label">
+            CLABE{" "}
+            <Tooltip title={textCLABE}>
+              <HelpCenterIcon />
+            </Tooltip>
+          </label>
+          <input
+            value={clabe}
+            onChange={(e) => setClabe(e.target.value)}
+            type="text"
+            className="form-control"
+            id="clabeUser"
+          ></input>
+        </div>
+        <input
+          onClick={updateUser}
+          className="btn btn-primary w-100"
+          type="submit"
+          value="Guardar datos"
+        ></input>
+      </form>
+    </>
+  );
 }
 
-export default ProfileEditableView
+export default ProfileEditableView;
