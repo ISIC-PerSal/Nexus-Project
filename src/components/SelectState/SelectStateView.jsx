4f0bbd90cbac9ffd67018b987f8f3c3ac2322e62
ISIC-PerSal <isic22.rpnrezs@itesco.edu.mx>
2024-08-21 17:27:59 -0600

    src/components/SelectState/SelectStateView.jsx

--- src/components/SelectState/SelectStateView.jsx
+++ src/components/SelectState/SelectStateView.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react";
 import Select from "react-select";
-function SelectStateView({estados, state, handleStateChange}) {
+
+function SelectStateView({ estados, state, handleStateChange }) {
   return (
     <>
       <div className="mb-3">
-        <label htmlFor="pais" className="form-label">Estado/Provincia/Región</label>
+        <label htmlFor="pais" className="form-label">
+          Estado/Provincia/Región
+        </label>
         <Select
           placeholder="--"
           options={estados}
           defaultValue={state}
           onChange={handleStateChange}
+          isSearchable={true}
         />
       </div>
     </>
-  )
+  );
 }
+
 export default SelectStateView;
