84cf0c30624e650bfa512f624c2397baf4e447e0
ErickPaul56 <isic22.ereyest@itesco.edu.mx>
2024-08-21 17:26:14 -0600

    src/components/SelectCountry/SelectCountryView.jsx

--- src/components/SelectCountry/SelectCountryView.jsx
+++ src/components/SelectCountry/SelectCountryView.jsx
@@ -1,23 +1,24 @@
 import Select from "react-select";
-import React from 'react'
-import PaisesProyecto from '../../util/location/PaisesProyecto'
+import React from "react";
+import PaisesProyecto from "../../util/location/PaisesProyecto";
 
-function SelectCountryView({handleCountryChange, country}) {
+function SelectCountryView({ handleCountryChange, country }) {
   return (
     <>
       <div className="mb-3">
-              <label htmlFor="pais" className="form-label">
-                País
-              </label>
-              <Select
-                placeholder="--"
-                options={PaisesProyecto}
-                defaultValue={country}
-                onChange={handleCountryChange}
-              />
-            </div>
+        <label htmlFor="pais" className="form-label">
+          País
+        </label>
+        <Select
+          placeholder="--"
+          options={PaisesProyecto}
+          defaultValue={country}
+          onChange={handleCountryChange}
+          isSearchable={true}
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default SelectCountryView
+export default SelectCountryView;
