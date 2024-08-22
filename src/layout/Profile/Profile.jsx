a34cf95eee89072d3a54e610b3bcc5f70d515671
ErickPaul56 <isic22.ereyest@itesco.edu.mx>
2024-08-21 18:39:20 -0600

    src\layout\Profile\Profile.jsx

--- src/layout/Profile/Profile.jsx
+++ src/layout/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ProfileView from "./ProfileView";
 import NavbarView from "../../components/Navbar/NavbarView";
 import calcularEdad from "../../util/user/calcularEdad";
+import fetchUpdate from "../../util/user/fetchUpdate";
 import Swal from "sweetalert2";
 
 function Profile() {
@@ -16,37 +17,46 @@ function Profile() {
 
   async function updateUser(e) {
     e.preventDefault();
-    const body = { 
-      idUser: sessionStorage.getItem("id_user"),
-      name: name,
-      lastName: lastName,
-      email: email,      
-      password: password,
-      birthday: birthday,
-      age: calcularEdad(birthday),
-      rfc: rfc,
-      clabe: clabe,
-    };
-    try {
-      const data = await fetchUpdate(body);
-      if (data.status == "Done") {
-        sessionStorage.setItem("id_user", body.id_user);
-        sessionStorage.setItem("email", body.email);
-        sessionStorage.setItem("name", body.name);
-        sessionStorage.setItem("lastName", body.lastName);
-        sessionStorage.setItem("birthday", body.birthday);
-        sessionStorage.setItem("age", body.age);
-        sessionStorage.setItem("password", body.password);
-        sessionStorage.setItem("rfc", body.rfc);
-        sessionStorage.setItem("clabe", body.clabe);
-        Swal.fire({
-          position: "top-end",
-          icon: "success",
-          title: "Los datos se han actualizados",
-          showConfirmButton: false,
-          timer: 1000,
-        });
-      } else {
+    const regex = new RegExp(/^[0-9]*$/);
+    if (rfc.trim().length > 11 && clabe.trim().length > 17 && regex.test(clabe)) {
+      const body = {
+        idUser: sessionStorage.getItem("id_user"),
+        name: name,
+        lastName: lastName,
+        email: email,
+        password: password,
+        birthday: birthday,
+        age: calcularEdad(birthday),
+        rfc: rfc.toUpperCase(),
+        clabe: clabe.toUpperCase(),
+      };
+      try {
+        const data = await fetchUpdate(body);
+        if (data.status == "Done") {
+          sessionStorage.setItem("email", body.email);
+          sessionStorage.setItem("name", body.name);
+          sessionStorage.setItem("lastName", body.lastName);
+          sessionStorage.setItem("birthday", body.birthday);
+          sessionStorage.setItem("age", body.age);
+          sessionStorage.setItem("password", body.password);
+          sessionStorage.setItem("rfc", body.rfc);
+          sessionStorage.setItem("clabe", body.clabe);
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Los datos se han actualizados",
+            showConfirmButton: false,
+            timer: 1000,
+          });
+        } else {
+          Swal.fire({
+            title: "Error!",
+            text: "Ocurrio un error",
+            icon: "error",
+            confirmButtonText: "OK",
+          });
+        }
+      } catch (error) {
         Swal.fire({
           title: "Error!",
           text: "Ocurrio un error",
@@ -54,16 +64,26 @@ function Profile() {
           confirmButtonText: "OK",
         });
       }
-    } catch (error) {
-      Swal.fire({
-        title: "Error!",
-        text: "Ocurrio un error",
-        icon: "error",
-        confirmButtonText: "OK",
-      });
+    } else {
+      if (rfc.trim().length < 12) {
+        Swal.fire({
+          position: "top-end",
+          icon: "info",
+          title: "Verifique los datos del RFC",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+      } else if (clabe.trim().length < 18 || regex.test(clabe)==false) {
+        Swal.fire({
+          position: "top-end",
+          icon: "info",
+          title: "Verifique los datos de la CLABE",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+      }
     }
   }
-
   return (
     <>
       <NavbarView />
@@ -76,7 +96,7 @@ function Profile() {
         setEmail={setEmail}
         birthday={birthday}
         setBirthday={setBirthday}
-        age:calcularEdad={birthday}
+        age={age}
         password={password}
         setPassword={setPassword}
         rfc={rfc}
