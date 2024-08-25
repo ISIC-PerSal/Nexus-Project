6d0e511a7657666a9b36207b554e464dd5cb2b67
ErickPaul56 <isic22.ereyest@itesco.edu.mx>
2024-08-25 12:42:48 -0600

    src\util\getDataApi.jsx

--- src/util/getDataApi.jsx
+++ src/util/getDataApi.jsx
@@ -1,30 +1,30 @@
-import React from "react";
+import React from 'react'
 
 async function getDataApi(url, body) {
-  try {
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
+    try {
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        if (body) {
+            options.body = JSON.stringify(body)
+        }
+        const response = await fetch(url, options)
+        console.log(response)
 
-    if (body) {
-      options.body = JSON.stringify(body);
+        if (!response.ok) {
+            throw new Error("Error en la petición")
+        }
+
+        const json = await response.json()
+        return json
+
+    } catch (error) {
+        console.error("Error: ", error.message)
+        return null
     }
-
-    const response = await fetch(url, options);
-
-    if (!response.ok) {
-      throw new Error("Error en la petición");
-    }
-
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    console.error("Error: ", error.message);
-    return null;
-  }
 }
 
-export default getDataApi;
+export default getDataApi
