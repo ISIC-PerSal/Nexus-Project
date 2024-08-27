768935c235bec20e7e1d053038f6dc43af912b80
ISIC-PerSal <isic22.rpnrezs@itesco.edu.mx>
2024-08-26 21:55:03 -0600

    src\components\CardProject\CardProjectView.jsx

--- src/components/CardProject/CardProjectView.jsx
+++ src/components/CardProject/CardProjectView.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import BadgeCountryView from '../BadgeLocation/BadgeCountryView';
+import React from "react";
+import odsData from "../../util/odsData";
+import BadgeODSView from "../BadgeODS/BadgeODSView";
+import BadgeCountryView from "../BadgeLocation/BadgeCountryView";
 
-function CardProjectView({ data }) {
-
-    return (
-        <>
-            <div className="card w-100 mb-3">
-                <img src={data.image} className="card-img" alt="..."></img>
-                <div className="card-body">
-                    <h5 className="card-title">{data.project}</h5>
-                    <BadgeCountryView data={data}/>
-                    <p className="card-text">{data.description}</p>
-                    <a href={data.url} className="btn btn-primary">Ir a la página oficial</a>
-                </div>
-            </div>
-        </>
-    );
+function CardProjectView({ data, ods }) {
+  return (
+    <>
+      <div className="card w-100 mb-3">
+        <img src={data.image} className="card-img-top"></img>
+        <div className="card-body">
+          <h5 className="card-title">{data.project}</h5>
+          <p className="card-text">{data.description}</p>
+          <div className="d-flex flex-wrap">
+          {ods.map((item, index) => (
+            <BadgeODSView key={index} item={item} />
+          ))}
+          </div>
+          <BadgeCountryView data={data}/>
+          <a href={data.url} className="btn btn-primary">
+            Ir a la página oficial
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default CardProjectView
+export default CardProjectView;
