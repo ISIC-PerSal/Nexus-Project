9b2bcf34f54e064d2a68814ffadca2e91a764001
LuisaLzrGcia <isic19.mlazarog@itesco.edu.mx>
2024-08-26 21:03:49 -0600

    jkj

--- src/components/BadgeLocation/BadgeCountryView.jsx
+++ src/components/BadgeLocation/BadgeCountryView.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from "react";
+import paises from "../../util/paises";
 
-function BadgeCountryView({data}) {
+function BadgeCountryView({ data }) {
+  const bandera = (item) => {
+    const pais = paises.find((pais) => pais.label.props.children[1] === item);
+
+    const imgSrc = pais.label.props.children[0].props.src;
+    console.log(imgSrc);
+    return imgSrc;
+  };
+
   return (
     <>
-            <span class="badge badge-info">
-                {data.country}
-            </span>
-        </>
-  )
+      <span class="badge text-bg-info ">
+        <div className="d-flex align-items-center">
+          {" "}
+          <img
+            src={bandera(data.country)}
+            alt=""
+            srcset=""
+            style={{ width: "30px" }}
+            className="me-1"
+          />
+          {data.country}
+        </div>
+      </span>
+    </>
+  );
 }
 
-export default BadgeCountryView
+export default BadgeCountryView;
