77a02f4b590e7a0106bfce6d3b12939ea4a6487c
ErickPaul56 <isic22.ereyest@itesco.edu.mx>
2024-08-28 20:02:45 -0600

    cambiosssssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
-import './App.css'
-import Login from './layout/Login/Login'
-import Register from './layout/Register/Register'
-import Home from './layout/Home/Home'
-import isAuth from './util/isAuth'
-import Main from './layout/Main/Main'
-import NewProject from './layout/New Project/NewProject'
-import About from './layout/About/About'
-import Directory from './layout/Directory/Directory'
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import Login from "./layout/Login/Login";
+import Register from "./layout/Register/Register";
+import Home from "./layout/Home/Home";
+import isAuth from "./util/isAuth";
+import Main from "./layout/Main/Main";
+import About from "./layout/About/About";
+import Directory from "./layout/Directory/Directory";
+import Alliance from "./layout/Alliance/Alliance";
+import Profile from "./layout/Profile/Profile";
+import NewProject from "./layout/NewProject/NewProject";
+import Explore from "./layout/Explore/Explore";
 
 function App() {
   const isLogin = isAuth() != null;
@@ -17,19 +25,31 @@ function App() {
     <>
       <Router>
         <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/directory" element={<Directory />} />
-        <Route path="/new-project" element={isLogin? <NewProject />:<Navigate to={"/login"}/>} />
-        <Route
-            path="/about"
-            element={isLogin ? <About/> : <Navigate to={"/login"} />}
+          <Route path="/" element={<Main />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/directory" element={<Directory />} />
+          <Route
+            path="/new-project"
+            element={isLogin ? <NewProject /> : <Navigate to={"/login"} />}
           />
+          <Route
+            path="/explore"
+            element={isLogin ? <Explore /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path="/about"
+            element={isLogin ? <About /> : <Navigate to={"/login"} />}
+          />
+          <Route path="/alliance" element={<Alliance />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/home" element={isLogin? <Home/>:<Navigate to={"/login"}/>}/>
+          <Route
+            path="/home"
+            element={isLogin ? <Home /> : <Navigate to={"/login"} />}
+          />
         </Routes>
       </Router>
     </>
   );
 }
-export default App
+export default App;
