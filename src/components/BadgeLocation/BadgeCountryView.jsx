ff6efaac357212503c7ca977bb5db7c364f0dfb9
ErickPaul56 <isic22.ereyest@itesco.edu.mx>
2024-08-28 20:18:50 -0600

    src/components/BadgeLocation/BadgeCountryView.jsx

---
+++ src/components/BadgeLocation/BadgeCountryView.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PaisesProyecto from "../../util/location/PaisesProyecto";
+
+function BadgeCountryView({ data }) {
+  const bandera = (item) => {
+    const pais = PaisesProyecto.find((pais) => pais.label.props.children[1] === item);
+
+    const imgSrc = pais.label.props.children[0].props.src;
+    return imgSrc;
+  };
+
+  return (
+    <>
+      <span className="badge text-bg-secondary ">
+        <div className="d-flex align-items-center">
+          {" "}
+          <img
+            alt=""
+            srcSet={bandera(data.country)}
+            style={{ width: "30px" }}
+            className="me-1"
+          />
+          {data.country}
+        </div>
+      </span>
+    </>
+  );
+}
+
+export default BadgeCountryView;
