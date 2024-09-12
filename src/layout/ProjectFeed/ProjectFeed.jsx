5a40ed269c36d3b0e40a34afa08e4fe721f84771
ISIC-PerSal <isic22.rpnrezs@itesco.edu.mx>
2024-09-11 19:57:50 -0600

    src/layout/ProjectFeed/ProjectFeed.jsx

--- src/layout/ProjectFeed/ProjectFeed.jsx
+++ src/layout/ProjectFeed/ProjectFeed.jsx
@@ -1,18 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import ProjectFeedView from "./ProjectFeedView";
 import { useNexus } from "../../Hooks/useContext";
+import { useParams } from "react-router-dom";
+import useProjectPermissions from "../../Hooks/useProjectPermissions";
+import fetchGetFeedByProject from "../../util/project/fetchGetFeedByProject";
 
 function ProjectFeed() {
   const { setSelected } = useNexus();
+  const { idProject } = useParams();
+  const permission = useProjectPermissions(idProject);
+  const [data, setData] = useState([]);
+
+  const getDataProject = async (body) => {
+    try {
+      const data = await fetchGetFeedByProject(body);
+      return data;
+    } catch (error) {
+      console.error("Ocurrió un error:", error);
+      return [];
+    }
+  };
 
   useEffect(() => {
     setSelected("Explorar");
   }, [setSelected]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const body = {
+        idProject: idProject,
+        status: "Privado",
+        type: "",
+      };
+      console.log(body)
+      const dataProject = await getDataProject(body);
+      setData(dataProject);
+    };
+
+    if (permission) {
+      fetchData();
+    }
+  }, [idProject, permission]);
+
   return (
     <>
       <Navbar />
-      <ProjectFeedView />
+      <ProjectFeedView data={data} />
     </>
   );
 }
