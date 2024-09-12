4437a54814950f58a4f33cab5103f55145a1139b
ErickPaul56 <isic22.ereyest@itesco.edu.mx>
2024-09-11 19:50:49 -0600

    src/components/GoFeed/GoFeed.jsx

--- src/components/GoFeed/GoFeed.jsx
+++ src/components/GoFeed/GoFeed.jsx
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from "react";
 import GoFeedView from "./GoFeedView";
-import fetchGetProjectsJoined from "../../util/project/fetchGetProjectsJoined";
-import isJoined from "../../util/project/isJoined";
 import useIsJoined from "../../util/project/isJoined";
+import fetchGetFeedByProject from "../../util/project/fetchGetFeedByProject";
+import { useNavigate } from "react-router-dom";
 
-function GoFeed({ idProject }) {
-  const [joinedUser, setJoinedUser] = useState(false);
-
+function GoFeed({ idProject, idUser }) {
+  const [status, setStatus] = useState("");
+  const currentUser = sessionStorage.getItem("id_user");
   const isJoined = useIsJoined(idProject);
+  const isOwner = currentUser == idUser;
+  const navigate = useNavigate();
 
-  
+  useEffect(() => {
+    if (isOwner) {
+      setStatus("Privado");
+    } else if (isJoined) {
+      setStatus("Miembro");
+    } else {
+      setStatus("Publico");
+    }
+  }, [idProject, idUser]);
 
-
-  const handleGoFeed = () => {};
+  const handleGoFeed = async () => {
+    window.location.href=`/feed/${idProject}`
+  };
   return (
     <>
-      <GoFeedView />
+      <GoFeedView handleGoFeed={handleGoFeed} />
     </>
   );
 }
