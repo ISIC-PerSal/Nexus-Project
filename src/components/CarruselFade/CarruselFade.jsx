bc46d28e7aa1d369e3bb35586af9d1a8b002bd37
ISIC-PerSal <isic22.rpnrezs@itesco.edu.mx>
2024-09-11 19:51:48 -0600

    src/components/CarruselFade/CarruselFade.jsx

---
+++ src/components/CarruselFade/CarruselFade.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CarruselFadeView from "./CarruselFadeView";
+
+function CarruselFade({ images }) {
+
+  return (
+    <>
+      <CarruselFadeView images = {images} />
+    </>
+  );
+}
+
+export default CarruselFade;
