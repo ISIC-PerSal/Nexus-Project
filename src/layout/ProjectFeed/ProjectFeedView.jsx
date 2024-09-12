51f9bcc5973aeea374451afeef42436b1c01d32c
JLeonardo96 <jesusleonardorc102@gmail.com>
2024-09-11 19:52:50 -0600

    src/layout/ProjectFeed/ProjectFeedView.jsx

--- src/layout/ProjectFeed/ProjectFeedView.jsx
+++ src/layout/ProjectFeed/ProjectFeedView.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import CardNews from "../../components/CardNews/CardNews";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import NoDataView from "../../components/NoData/NoDataView";
 
-function ProjectFeedView() {
+function ProjectFeedView({ data = [] }) {
+  console.log(data)
   return (
-    <div>ProjectFeedView</div>
-  )
+    <>
+      <main className="container w-75 p-3  my-3 border rounded border-secondary-subtle bg-gral">
+        {data.length ? (
+          <Masonry columnsCount={3} gutter="10px">
+            {data.map((item, index) => (
+              <CardNews item={item} key={index} />
+            ))}
+          </Masonry>
+        ) : (
+          <NoDataView />
+        )}
+      </main>
+    </>
+  );
 }
 
-export default ProjectFeedView
+export default ProjectFeedView;
