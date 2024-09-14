f90bf06d35e067293ef26fcdee02206a2bfe34cc
JLeonardo96 <jesusleonardorc102@gmail.com>
2024-09-13 19:48:47 -0600

    src/components/CardNews/CardNews.jsx

--- src/components/CardNews/CardNews.jsx
+++ src/components/CardNews/CardNews.jsx
@@ -1,41 +1,22 @@
 import React from "react";
 import CardNewsView from "./CardNewsView";
+import { useNavigate } from "react-router-dom";
+import { getStatusIcon } from "../../util/Icons/getStatusIcon";
 
 function CardNews({ item }) {
-  const getStatusIcon = (status) => {
-    switch (status) {
-      case "Privado":
-        return <span className="badge bg-azul-1 ms-1">
-          <div className="d-flex align-items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-lock2" viewBox="0 0 16 16">
-            <path d="M10 7v1.076c.54.166 1 .597 1 1.224v2.4c0 .816-.781 1.3-1.5 1.3h-3c-.719 0-1.5-.484-1.5-1.3V9.3c0-.627.46-1.058 1-1.224V7a2 2 0 1 1 4 0M7 7v1h2V7a1 1 0 0 0-2 0" />
-            <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
-          </svg>
-          <span className="ms-1">{status}</span>
-          </div>
-        </span>
-      case "Miembros":
-        return <span className="badge bg-azul-2 ms-1">
-          <div className="d-flex align-items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-check-fill" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0" />
-            <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-          </svg>
-          <span className="ms-1">{status}</span>
-          </div>
-        </span>
-      default:
-        return <span className="badge bg-azul-3 ms-1">
-          <div className="d-flex align-items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
-          <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
-        </svg>
-          <span className="ms-1">{status}</span>
-          </div>
-        </span>
-    }
+  const statusIcon = getStatusIcon(item.status);
+  const Navidate = useNavigate();
+
+  const handleToFeedContent = () => {
+    Navidate(`/feed/${item.id_project_fk}/${item.code_project}`);
   };
-  return <CardNewsView item={item} getStatusIcon={getStatusIcon} />;
+  return (
+    <CardNewsView
+      item={item}
+      statusIcon={statusIcon}
+      handleToFeedContent={handleToFeedContent}
+    />
+  );
 }
 
 export default CardNews;
