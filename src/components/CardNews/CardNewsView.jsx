ae719a21e62cb3180db887c671c1e4c10f1cab4c
JLeonardo96 <jesusleonardorc102@gmail.com>
2024-09-13 19:49:13 -0600

    src/components/CardNews/CardNewsView.jsx

--- src/components/CardNews/CardNewsView.jsx
+++ src/components/CardNews/CardNewsView.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CarruselFade from "../CarruselFade/CarruselFade";
 
-function CardNewsView({ item, getStatusIcon }) {
+function CardNewsView({ item, statusIcon, handleToFeedContent }) {
   const dateSplit = item.created_at.split(" ")[0];
   const dateArray = dateSplit.split("-");
   const date = dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
@@ -10,36 +10,81 @@ function CardNewsView({ item, getStatusIcon }) {
     <div className="card w-100">
       {item.files.length > 0 ? <CarruselFade images={item.files} /> : ""}
       <div className="card-body">
-        {
-          item.type == "Noticia" ?
-            (<span className="badge text-bg-success">{item.type}</span>) :
-            (<span className="badge text-bg-warning">{item.type}</span>)
-        }
-        {getStatusIcon(item.status)}
-        <div className="d-flex align-items-center">
-          <span class="badge text-bg-secondary ms-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar4-week me-1" viewBox="0 0 16 16">
-            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z" />
-            <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
-          </svg>
-            <span className="ms.1">{date}</span>
+        {item.type == "Noticia" ? (
+          <span className="badge text-bg-success mb-1">
+            <div className="d-flex align-items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-newspaper"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z" />
+                <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z" />
+              </svg>
+              <span className="ms-1">{item.type}</span>
+            </div>
           </span>
-          <div />
-          <div className="d-flex align-items-center"></div>
-          <span class="badge text-bg-secondary ms-1">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clock-fill me-1" viewBox="0 0 16 16">
+        ) : (
+          <span className="badge text-bg-warning mb-1">
+            <div className="d-flex align-items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-card-checklist"
+                viewBox="0 0 16 16"
+              >
+                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z" />
+                <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0" />
+              </svg>
+              <span className="ms-1">{item.type}</span>
+            </div>
+          </span>
+        )}
+        {statusIcon}
+        <span className="badge text-bg-secondary ms-1">
+          <div className="d-flex align-items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              ss="bi bi-calendar4-week"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z" />
+              <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
+            </svg>
+            <span className="ms-1">{date}</span>
+          </div>
+        </span>
+        <span className="badge text-bg-secondary ms-1">
+          <div className="d-flex align-items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-clock-fill"
+              viewBox="0 0 16 16"
+            >
               <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
             </svg>
             <span className="ms-1">{time}</span>
-          </span>
-        </div>
-        <di
+          </div>
+        </span>
+        <div
           className="card-text truncated-text"
           dangerouslySetInnerHTML={{
             __html: item.activity_content || "No content available",
           }}
         />
-        <a href="#" className="btn bg-oficial text-white">
-          Ver mas
+        <a onClick={handleToFeedContent} className="btn bg-oficial text-white">
+          Ver más
         </a>
       </div>
     </div>
