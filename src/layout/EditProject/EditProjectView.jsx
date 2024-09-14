08e7f901ac56fc61596ecd3bde58c21113ba273e
JLeonardo96 <jesusleonardorc102@gmail.com>
2024-09-13 21:42:03 -0600

    src\layout\EditProject\EditProjectView.jsx

--- src/layout/EditProject/EditProjectView.jsx
+++ src/layout/EditProject/EditProjectView.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-
-function EditProjectView({ data, project, setProject, image, setImage, leader_type, setLeader_type, name, setName, ods1, setOds1 }) {
-    return (
-        <>
-            <main className="py-3 container w-50 mt-3 p-3 border rounded border-secondary-subtle bg-gral">
-                <h1>Editar Projecto</h1>
-                <form
-                    action=""
-                >
-                    <div className="mb-3">
-                        <label htmlFor="nombre-proyecto" className="form-label">
-                            Nombre del líder o representante
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="nombre-proyecto"
-                            value={project}
-                            onChange={(e) => setProject(e.target.value)}
-                        ></input>
-                    </div>
-                </form>
-            </main>
-        </>
-    )
+function EditProjectView({ data, updateProjectData }) {
+  const handleChange = (field) => (event) => {
+    updateProjectData(field, event.target.value);
+  };
+  console.log(data);
+  return (
+    <>
+      <main className="py-3 container w-50 mt-3 p-3 border rounded border-secondary-subtle bg-gral">
+        <h1>Editar Projecto</h1>
+        <form action="">
+          <div className="mb-3">
+            <label htmlFor="nombre-lider" className="form-label">
+              Nombre del líder o representante
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="nombre-lider"
+              value={data.project}
+              onChange={(e) => handleChange("project")}
+            ></input>
+          </div>
+        </form>
+      </main>
+    </>
+  );
 }
 
-export default EditProjectView
+export default EditProjectView;
