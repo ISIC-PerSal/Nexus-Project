49e828c9869dcd756553fcd949a8bec05b060869
JLeonardo96 <jesusleonardorc102@gmail.com>
2024-09-18 20:46:46 -0600

    src/util/user/fetchRegister.jsx

--- src/util/user/fetchRegister.jsx
+++ src/util/user/fetchRegister.jsx
@@ -3,12 +3,13 @@ import getDataApi from '../getDataApi';
 const API_NEXUS_PROJECT= import.meta.env.VITE_API_NEXUS_PROJECT_DATABASE;
 
 async function fetchRegister(body) {
+    console.log(body)
     const url = `${API_NEXUS_PROJECT}/user/createUser.php`;
     try {
         const data = await getDataApi(url, body);
         return data
     } catch (error) {
-        console.error("Error: ",error.message)
+        console.error("Error: ", error.message)
         return null
     }
 }
