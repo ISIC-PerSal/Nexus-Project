14e39d1e59346f2ee372dbdd7242c0056feaeb7b
LuisaLzrGcia <isic19.mlazarog@itesco.edu.mx>
2024-09-18 20:13:59 -0600

    src\components\SelectCity\SelectCity.jsx

--- src/components/SelectCity/SelectCity.jsx
+++ src/components/SelectCity/SelectCity.jsx
@@ -7,6 +7,20 @@ import bra_cities from "../../util/location/bra_cities";
 import bolivia_cities from "../../util/location/bolivia_cities";
 import Chile_cities from "../../util/location/Chile_cities";
 import colombia_cities from "../../util/location/colombia_cities";
+import costa_rica_cities from "../../util/location/costa_rica_cities";
+import cuba_cities from "../../util/location/cuba_cities";
+import ecuador_cities from "../../util/location/ecuador_cities";
+import elsalvador_cities from "../../util/location/elsalvador_cities";
+import guatemala_cities from "../../util/location/guatemala_cities";
+import honduras_cities from "../../util/location/honduras_cities";
+import nicaragua_cities from "../../util/location/nicaragua_cities";
+import panama_cities from "../../util/location/panama_cities";
+import paraguay_cities from "../../util/location/paraguay_cities";
+import peru_cities from "../../util/location/peru_cities";
+import republica_dominicana_cities from "../../util/location/republica_dominicana_cities";
+import uruguay_cities from "../../util/location/uruguay_cities";
+import venezuela_cities from "../../util/location/venezuela_cities";
+import puerto_rico_cities from "../../util/Location/puerto_rico_cities";
 
 function SelectCity({ city, setCity, country, state }) {
   const [cities, setCities] = useState([]);
@@ -32,7 +46,7 @@ function SelectCity({ city, setCity, country, state }) {
         return (
           Chile_cities.find((item) => item.Estado === estado)?.Ciudades || []
         );
-        case "Colombia":
+      case "Colombia":
         return (
           colombia_cities.find((item) => item.Estado === estado)?.Ciudades || []
         );
@@ -40,6 +54,69 @@ function SelectCity({ city, setCity, country, state }) {
         return (
           mex_cities.find((item) => item.Estado === estado)?.Ciudades || []
         );
+      case "Costa Rica":
+        return (
+          costa_rica_cities.find((item) => item.Estado === estado)?.Ciudades ||
+          []
+        );
+      case "Cuba":
+        return (
+          cuba_cities.find((item) => item.Estado === estado)?.Ciudades || []
+        );
+      case "Ecuador":
+        return (
+          ecuador_cities.find((item) => item.Estado === estado)?.Ciudades || []
+        );
+      case "El Salvador":
+        return (
+          elsalvador_cities.find((item) => item.Estado === estado)?.Ciudades ||
+          []
+        );
+      case "Guatemala":
+        return (
+          guatemala_cities.find((item) => item.Estado === estado)?.Ciudades ||
+          []
+        );
+      case "Honduras":
+        return (
+          honduras_cities.find((item) => item.Estado === estado)?.Ciudades || []
+        );
+      case "Nicaragua":
+        return (
+          nicaragua_cities.find((item) => item.Estado === estado)?.Ciudades ||
+          []
+        );
+      case "Panamá":
+        return (
+          panama_cities.find((item) => item.Estado === estado)?.Ciudades || []
+        );
+      case "Paraguay":
+        return (
+          paraguay_cities.find((item) => item.Estado === estado)?.Ciudades || []
+        );
+      case "Perú":
+        return (
+          peru_cities.find((item) => item.Estado === estado)?.Ciudades || []
+        );
+        case "Puerto Rico":
+          return (
+            puerto_rico_cities.find((item) => item.Estado === estado)?.Ciudades || []
+          );
+      case "República Dominicana":
+        return (
+          republica_dominicana_cities.find((item) => item.Estado === estado)
+            ?.Ciudades || []
+        );
+      case "Uruguay":
+        return (
+          uruguay_cities.find((item) => item.Estado === estado)?.Ciudades || []
+        );
+      case "Venezuela":
+        return (
+          venezuela_cities.find((item) => item.Estado === estado)?.Ciudades ||
+          []
+        );
+
       default:
         return [];
     }
