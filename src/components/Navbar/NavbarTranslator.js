f712a6454682df343e550b4ef86e18b04ff4c2c1
JLeonardo96 <jesusleonardorc102@gmail.com>
2024-11-08 19:42:59 -0600

    Cambios 08/11/2024

---
+++ src/components/Navbar/NavbarTranslator.js
@@ -0,0 +1,30 @@
+const NavbarTranslator = {
+    spanish: {
+        news: "Noticias",
+        directory: "Directorio",
+        register: "Registar Proyecto",
+        explore: "Explorar proyectos",
+        alliance: "Alianza juvenil e infantil",
+        aboutUs: "Acerca de nosotros",
+        me: "Yo",
+        profile: "Mi perfil",
+        project: "Mis proyectos",
+        support: "Soporte",
+        logOut: "Cerrar sesión",
+    },
+    english: {
+        news: "News",
+        directory: "Directory",
+        register: "Register project",
+        explore: "Explore projects",
+        alliance: "Youth and children's alliance",
+        aboutUs: "About us",
+        me: "Me",
+        profile: "My profile",
+        project: "My project",
+        support: "Support",
+        logOut: "Log out",
+    },
+};
+
+export default NavbarTranslator;
---
+++ src/layout/Directory/DirectoryTranslator.js
@@ -0,0 +1,29 @@
+const DirectoryTranslator = {
+    spanish: {
+        title: "Directorio",
+        blockQuote: "Nunca subestimes el poder de un grupo de personas comprometidas para cambiar el mundo. De hecho, es lo único que lo ha logrado.",
+        author: "Margaret Mead",
+        occupation: ", antropóloga cultural estadounidense.",
+        description: <>"Nexus Project se fundamenta en los{" "}
+        <b style={{ color: "#A63F52" }}>nexos</b> entre los miembros de una
+        comunidad para lograr objetivos por el bienestar común. En la
+        sección <b>Directorio</b> hemos reunido enlaces de contacto a
+        aliados poderosos con los que nuestros usuarios podrían amplificar
+        el impacto de sus ideas si estos las conocieran."</>,
+        selectCountry: "Seleccione un país",
+        letterFilter: "Filtrar por letra inicial",
+        sdgFilter: "Filtrar por ODS",
+    },
+    english: {
+        title: "Directory",
+        blockQuote: "Never underestimate the power of a group of committed people to change the world. In fact, it's the only thing that ever has.",
+        author: "Margaret Mead",
+        occupation: ", American cultural anthropologist.",
+        description: "Nexus Project is based on the",
+        selectCountry: "Select a country",
+        letterFilter: "Filter by initial letter",
+        sdgFilter: "Filter by SDG",
+    },
+};
+
+export default DirectoryTranslator;
